a59a743ce072c7e9373db6983c6ce0f3c1430b88
Luke Baltay <luke.baltay@yale.edu>
2022-07-12 10:35:03 -0400

    commit tree 672c6e4dd3b465f8e395972fea487a675161381c

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+function together(num1, num2) {
+    return num1 * num2;
+}
+
+let multiply = together(num1, num2);
+
+// function random() {
+//     return Math.ciel(Math.random() * 100);
+// }
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 64;
+const num4 = 5;
+
+function remainder(num3, num4) {
+    return num3 % num4;
+}
+
+let mod = remainder(num3, num4);
+
+const set1 = 1;
+const set2 = 3;
+const set4 = 20;
+
+
+let max = Math.max(set1, set2, set4);
